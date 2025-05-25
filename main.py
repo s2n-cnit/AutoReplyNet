fd14184af6a1ffaae03a5f53e642336241559bbc
Alex Carrega <alessandro.carrega@gmail.com>
2025-05-25 16:14:18 +0200

    Automatic commit with GitManager

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import os
 import socket
 import sys
 import threading
+from random import randint
+from typing import Tuple
 
 running = True
 
 
-def generate_random_reply() -> str | bytes:
-    return os.urandom(10)
+def generate_random_reply(packet_size: Tuple[int, int]) -> str | bytes:
+    size = randint(*packet_size)
+    return os.urandom(size)
 
 
 def to_bytes(text: str | bytes) -> bytes:
@@ -17,7 +20,7 @@ def to_bytes(text: str | bytes) -> bytes:
     return text
 
 
-def handle_tcp_client(conn, addr):
+def handle_tcp_client(conn, addr, packet_size: Tuple[int, int]):
     """Handles a single TCP client connection."""
     print(f"[TCP] Connected by {addr}")
     try:
@@ -29,7 +32,7 @@ def handle_tcp_client(conn, addr):
             received_message = data.decode('utf-8', errors='ignore')
             print(f"[TCP] Received from {addr}: {received_message[:100]}")  # Print first 100 chars
 
-            reply_message = generate_random_reply()
+            reply_message = generate_random_reply(packet_size)
             print(f"[TCP] Replying to {addr}: {reply_message}")
             conn.sendall(to_bytes(reply_message))
 
@@ -42,7 +45,7 @@ def handle_tcp_client(conn, addr):
         conn.close()
 
 
-def start_tcp_server(host: str, port: int) -> None:
+def start_tcp_server(host: str, port: int, packet_size: Tuple[int, int]) -> None:
     """Starts the TCP server."""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -52,11 +55,11 @@ def start_tcp_server(host: str, port: int) -> None:
 
         while running:
             conn, addr = s.accept()
-            client_handler = threading.Thread(target=handle_tcp_client, args=(conn, addr))
+            client_handler = threading.Thread(target=handle_tcp_client, args=(conn, addr, packet_size))
             client_handler.start()
 
 
-def start_udp_server(host: str, port: int) -> None:
+def start_udp_server(host: str, port: int, packet_size: Tuple[int, int]) -> None:
     """Starts the UDP server."""
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.bind((host, port))
@@ -68,7 +71,7 @@ def start_udp_server(host: str, port: int) -> None:
                 received_message = data.decode('utf-8', errors='ignore')
                 print(f"[UDP] Received from {addr}: {received_message[:100]}")  # Print first 100 chars
 
-                reply_message = generate_random_reply()
+                reply_message = generate_random_reply(packet_size)
                 print(f"[UDP] Replying to {addr}: {reply_message}")
                 s.sendto(to_bytes(reply_message), addr)  # Send reply back to sender
             except Exception as e:
@@ -83,14 +86,18 @@ if __name__ == "__main__":
                         help="Specify the port to listen")
     parser.add_argument('--host', '-o', type=str, default="0.0.0.0",
                         help="Specify the port to listen")
+    parser.add_argument('--min-packet-size', '-n', type=int, default=20,
+                        help="Minimum packet (only payload) size of the response")
+    parser.add_argument('--max-packet-size', '-m', type=int, default=1000,
+                        help="Maximum packet (only payload) size of the response")
 
     args = parser.parse_args()
 
     try:
         if args.protocol == 'tcp':
-            start_tcp_server(host=args.host, port=args.port)
+            start_tcp_server(host=args.host, port=args.port, packet_size=(args.min_packet_size, args.max_packet_size))
         elif args.protocol == 'udp':
-            start_udp_server(host=args.host, port=args.port)
+            start_udp_server(host=args.host, port=args.port, packet_size=(args.min_packet_size, args.max_packet_size))
         else:
             print("Invalid protocol specified. Use 'tcp' or 'udp'.")
             sys.exit(1)
